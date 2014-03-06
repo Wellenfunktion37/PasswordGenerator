55357e1051a977071c5721d5a79d30a0fd9146bd
Philipp <pbutt84@gmail.com>
2014-03-06 18:07:19 +0100

    added a textArea, WIP: weird Layout Problems

--- src/Testclass.java
+++ src/Testclass.java
@@ -1,3 +1,5 @@
+
+
 import java.awt.*;
 import javax.swing.*;
 
@@ -10,21 +12,42 @@ public class Testclass extends JFrame {
 
     Container c;
     JComboBox passwordSpecies;
+    JTextArea passwordScreen;
+    JScrollPane sp;
+
 
     public Testclass() {
         c = getContentPane();
-        c.setLayout(new FlowLayout());
+        c.setLayout(new BorderLayout());
+
+        passwordScreen = new JTextArea("Space to show passwords");
+
+
+        //Sets Font
+        Font font = new Font("Arial", Font.PLAIN , 12);
+        passwordScreen.setFont(font);
+        sp = new JScrollPane(passwordScreen);
+
+        //Sets page layout
+
+        passwordScreen.setLineWrap(true);
+        passwordScreen.setWrapStyleWord(true);
+
+        //adds the components
+        c.add(passwordScreen, BorderLayout.CENTER);
+        c.add(sp);
+
 
         String[] species = new String[] {"Letters", "Numbers", "Good to remember"};
         passwordSpecies = new JComboBox(species);
-        c.add(passwordSpecies);
+        c.add(passwordSpecies, BorderLayout.NORTH);
     }
 
 
     public static void main(String[] args) {
         Testclass Window = new Testclass();
         Window.setTitle("Password Generator");
-        Window.setSize(240 ,160);
+        Window.setSize(240, 160);
         Window.setVisible(true);
         Window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
